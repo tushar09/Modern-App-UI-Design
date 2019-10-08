954cd24bd05bc0377a3083c59f063a4dd30666fc
Tushar <wtushar09@gmail.com>
2019-10-08 09:40:18 +0600

    fullscreen, custom edittext

--- app/src/main/java/com/aaroza/classroom/newui/activity/login/Login1Activity.java
+++ app/src/main/java/com/aaroza/classroom/newui/activity/login/Login1Activity.java
@@ -2,7 +2,9 @@ package com.aaroza.classroom.newui.activity.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.aaroza.classroom.newui.R;
 
@@ -13,5 +15,11 @@ public class Login1Activity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
         getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
     }
 }
