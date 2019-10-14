769085cb24f4b069d7d77ded7667a976ed278d44
Tushar <wtushar09@gmail.com>
2019-10-14 13:43:16 +0600

    retrofit is functional now

--- app/src/main/java/com/aaroza/classroom/newui/utils/Constants.java
+++ app/src/main/java/com/aaroza/classroom/newui/utils/Constants.java
@@ -1,5 +1,34 @@
 package com.aaroza.classroom.newui.utils;
 
+import com.aaroza.classroom.newui.services.ApiService;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Constants{
     public static String BASE_URL = "http://192.168.10.17:3000/";
+    private static ApiService apiService;
+
+    public static final ApiService getApiService() {
+        if (apiService == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.connectTimeout(5, TimeUnit.MINUTES);
+            httpClient.readTimeout(5, TimeUnit.MINUTES);
+            httpClient.addInterceptor(logging);
+            apiService = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
+                    .build()
+                    .create(ApiService.class);
+        }
+        return apiService;
+    }
 }
